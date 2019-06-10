ed439b9f1822418c411f0b415765807b8c93cc7d
HarisQadri <syedharis1540@gmail.com>
2019-06-10 18:42:11 +0500

    Add project files.

---
+++ userpanel/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace userpanel.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult CareerWithUs()
+        {
+            return View();
+        }
+
+        public ActionResult OurBusiness()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ userpanel/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(userpanel.Startup))]
+namespace userpanel
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
